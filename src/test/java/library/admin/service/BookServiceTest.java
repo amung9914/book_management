cb1a8ac1f2331934eb4ec52767323a38c4b0cf26
amung9914 <rlatjdud511@naver.com>
2023-12-20 15:24:26 +0900

    fix: build시 test 코드 제외
    @Disabled 코드 사용

---
+++ src/test/java/library/admin/service/BookServiceTest.java
@@ -0,0 +1,103 @@
+package library.admin.service;
+
+import library.admin.domain.BookVO;
+import library.admin.domain.MemberVO;
+import library.admin.repository.BookMapper;
+import library.admin.repository.RecordMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class BookServiceTest {
+
+    @Autowired BookService bookService;
+    @Autowired
+    BookMapper bookMapper;
+    @Autowired
+    RecordMapper recordMapper;
+    @Autowired MemberService memberService;
+
+
+    @BeforeEach
+    @Disabled
+    public void clearMemberRepository(){
+        recordMapper.deleteAll();
+        bookMapper.deleteAll();
+
+    }
+
+    @DisplayName("도서를 등록한다")
+    @Test
+    @Disabled
+    public void register() throws Exception {
+        // given
+        BookVO newBook = BookVO.builder()
+                .bookName("testbook")
+                .author("testauthor")
+                .build();
+        // when
+        bookService.register(newBook);
+
+        // then
+        Assertions.assertThat(bookService.bookList().get(0).getBookName()).isEqualTo(newBook.getBookName());
+    }
+
+    @DisplayName("도서를 수정한다")
+    @Test
+    @Disabled
+    public void testMethodNameHere() throws Exception {
+        // given
+        BookVO newBook = BookVO.builder()
+                .bookName("testbook")
+                .author("testauthor")
+                .build();
+        bookService.register(newBook);
+
+        // when
+        BookVO findBook = bookService.bookList().get(0);
+        int findId = findBook.getBookId();
+
+        BookVO updateBook = BookVO.builder()
+                            .bookId(findId)
+                            .bookName("newBook")
+                            .author("testauthor")
+                            .build();
+        bookService.update(updateBook);
+
+        // then
+        Assertions.assertThat(bookService.bookList().get(0).getBookName()).isEqualTo(updateBook.getBookName());
+    }
+
+    @DisplayName("도서를 대출한다")
+    @Test
+    @Disabled
+    public void borrow() throws Exception {
+        // given
+        BookVO newBook = BookVO.builder()
+                .bookName("testbook")
+                .author("testauthor")
+                .build();
+        bookService.register(newBook);
+        int bookId = bookService.bookList().get(0).getBookId();
+
+        MemberVO member = memberService.findMemberbyName("test1");
+
+        // when
+        bookService.borrow(bookId,member);
+
+        // then
+        Assertions.assertThat(bookService.bookList().get(0).getBookStatus()).isEqualTo("BORROWED");
+        Assertions.assertThat(recordMapper.findByBookId(bookId).getBorrowTime()).isBefore(LocalDateTime.now());
+    }
+
+
+
+}
